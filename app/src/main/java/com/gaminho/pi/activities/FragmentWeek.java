cc60f9d438c5252122a1bbde9391f23aa296af2a
ariche <riche.ant@gmail.com>
2018-08-14 17:57:59 +0200

    Update daily views and earned money when there is courses inside ViewPager.

--- app/src/main/java/com/gaminho/pi/activities/FragmentWeek.java
+++ app/src/main/java/com/gaminho/pi/activities/FragmentWeek.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,14 +20,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class FragmentWeek extends FirebaseFragment {
 
-    private TextView mTVCounter;
     private ViewPager mViewPager;
-
 
     public FragmentWeek() {
     }
@@ -39,7 +40,6 @@ public class FragmentWeek extends FirebaseFragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_week, container, false);;
-        mTVCounter = view.findViewById(R.id.tv_foreseen_class_count);
         mViewPager = view.findViewById(R.id.viewpager);
         return view;
     }
@@ -55,25 +55,41 @@ public class FragmentWeek extends FirebaseFragment {
         viewPager.setAdapter(m);
     }
 
-
     @Override
     protected void refreshView() {
-        mListItems.clear();
-        mListItems = ((ArrayList<Course>) mListener.getItems(ListItemFragment.LIST_COURSE)).stream().filter(
-                course -> new Date(course.getDate()).after(new Date())
-        ).collect(Collectors.toList());
-
-        mTVCounter.setText(String.format(Locale.FRANCE, "%d foreseen courses", mListItems.size()));
-        double money = 0;
-
-        for(Course course : ((ArrayList<Course>)mListItems)){
-            if (course.getMoney() > 0)
-                money += course.getMoney();
-        }
-
-        mTVCounter.append(String.format(Locale.FRANCE, "\nExpected money: %.2f €", money));
+//        mListItems.clear();
+//        mListItems = ((ArrayList<Course>) mListener.getItems(ListItemFragment.LIST_COURSE)).stream().filter(
+//                course -> new Date(course.getDate()).after(new Date())
+//        ).collect(Collectors.toList());
+//
+//        mTVCounter.setText(String.format(Locale.FRANCE, "%d foreseen courses", mListItems.size()));
+//        double money = 0;
+//
+//        for(Course course : ((ArrayList<Course>)mListItems)){
+//            if (course.getMoney() > 0)
+//                money += course.getMoney();
+//        }
+//
+//        mTVCounter.append(String.format(Locale.FRANCE, "\nExpected money: %.2f €", money));
     }
 
+    protected static Date[] getWeekSlot(int pWeekOffset){
+        Date[] slot = new Date[2];
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+        calendar.clear(Calendar.MINUTE);
+        calendar.clear(Calendar.SECOND);
+        calendar.clear(Calendar.MILLISECOND);
+        calendar.add(Calendar.DAY_OF_YEAR, pWeekOffset * 7);
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+        slot[0] = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 6);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        slot[1] = calendar.getTime();
+        return slot;
+    }
 
     public static class MyAdapter extends FragmentStatePagerAdapter {
 
@@ -95,10 +111,15 @@ public class FragmentWeek extends FirebaseFragment {
     }
 
 
-    public static class FragmentAWeek extends Fragment {
+    public static class FragmentAWeek extends FirebaseFragment {
 
         private int mOffset;
         private Date mStartDate, mEndDate;
+        private TextView mTVCourses, mTVMoney;
+        private List<CardView> mCVDays = new ArrayList<>();
+        private static final int ONE_DAY = 1000 * 60 * 60 * 24;
+
+
 
         public FragmentAWeek() {
         }
@@ -111,6 +132,10 @@ public class FragmentWeek extends FirebaseFragment {
             return fragment;
         }
 
+        private List<Course> getListCourses(){
+            return (ArrayList<Course>) mListItems;
+        }
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -118,20 +143,25 @@ public class FragmentWeek extends FirebaseFragment {
             //Retrieving offset of week
             mOffset = getArguments() != null ? getArguments().getInt("offset") : 0;
 
-            //Getting slot dates
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
-            calendar.clear(Calendar.MINUTE);
-            calendar.clear(Calendar.SECOND);
-            calendar.clear(Calendar.MILLISECOND);
-            calendar.add(Calendar.DAY_OF_YEAR, mOffset * 7);
-            calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
-            mStartDate = calendar.getTime();
+            mStartDate = getWeekSlot(mOffset)[0];
+            mEndDate = getWeekSlot(mOffset)[1];
+        }
 
-            calendar.add(Calendar.DAY_OF_YEAR, 6);
-            calendar.set(Calendar.HOUR_OF_DAY, 23);
-            calendar.set(Calendar.MINUTE, 59);
-            mEndDate = calendar.getTime();
+        @Override
+        protected void refreshView() {
+            mListItems.clear();
+            getListCourses().addAll(((ArrayList<Course>) mListener.getItems(ListItemFragment.LIST_COURSE))
+                    .stream().filter(course -> new Date(course.getDate()).after(this.mStartDate)
+                            && new Date(course.getDate()).before(this.mEndDate))
+                    .sorted((course1, course2) -> Long.valueOf(course1.getDate()).compareTo(course2.getDate()))
+                    .collect(Collectors.toList())
+            );
+
+            double money = getListCourses().stream().mapToDouble(Course::getMoney).sum();
+
+            mTVCourses.setText(String.format(Locale.FRANCE, "%d", mListItems.size()));
+            mTVMoney.setText(String.format(Locale.FRANCE, "%.2f", money));
+            fillDayViews();
         }
 
         @Override
@@ -140,14 +170,73 @@ public class FragmentWeek extends FirebaseFragment {
 
             View view = inflater.inflate(R.layout.a_week, container, false);
 
-            String label = String.format(Locale.FRANCE, "Lu. %s - Di. %s",
-                    new SimpleDateFormat("dd-MM-yyyy").format(mStartDate),
-                    new SimpleDateFormat("dd-MM-yyyy").format(mEndDate));
+            mTVCourses = view.findViewById(R.id.tv_nb_courses);
+            mTVMoney = view.findViewById(R.id.tv_nb_money);
 
-            ((TextView) view.findViewById(R.id.tv_week_label)).setText(label);
+            mCVDays.add(view.findViewById(R.id.lay_day_1));
+            mCVDays.add(view.findViewById(R.id.lay_day_2));
+            mCVDays.add(view.findViewById(R.id.lay_day_3));
+            mCVDays.add(view.findViewById(R.id.lay_day_4));
+            mCVDays.add(view.findViewById(R.id.lay_day_5));
+            mCVDays.add(view.findViewById(R.id.lay_day_6));
+            mCVDays.add(view.findViewById(R.id.lay_day_7));
 
-//            mTVLabel = view.findViewById(R.id.tv_week_label);
+            Calendar cal = Calendar.getInstance();
+            int currentDay = cal.get(Calendar.DAY_OF_YEAR);
+            cal.setTime(mStartDate);
+
+            for(CardView cv : mCVDays) {
+                ((TextView) cv.findViewById(R.id.tv_day_month)).setText(
+                        new SimpleDateFormat("MMM", Locale.FRANCE).format(cal.getTime()));
+                ((TextView) cv.findViewById(R.id.tv_day_label)).setText(
+                        new SimpleDateFormat("EE", Locale.FRANCE).format(cal.getTime()));
+                ((TextView) cv.findViewById(R.id.tv_day_number)).setText(
+                        String.format(Locale.FRANCE, "%02d", cal.get(Calendar.DAY_OF_MONTH)));
+
+                if(currentDay == cal.get(Calendar.DAY_OF_YEAR)){
+                    cv.findViewById(R.id.tv_day_lay).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                    ((TextView) cv.findViewById(R.id.tv_day_number)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                    ((TextView) cv.findViewById(R.id.tv_day_label)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                    ((TextView) cv.findViewById(R.id.tv_day_month)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
+
+                cal.add(Calendar.DAY_OF_YEAR, 1);
+            }
+
             return view;
+        }
+
+        private void fillDayViews(){
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH'h'MM");
+
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(mStartDate);
+
+            for(CardView cv : mCVDays) {
+
+                List<Course> courses = filterCourseByDay(getListCourses(), cal.getTimeInMillis());
+
+                if(!courses.isEmpty()){
+                    String str = courses.stream().map(course ->
+                            String.format(
+                                    Locale.FRANCE, "%s - %s : %s %s",
+                                    sdf.format(new Date(course.getDate())),
+                                    sdf.format(new Date(course.getDate() + course.getDuration())),
+                                    course.getPupil().getFirstname(), course.getPupil().getLastname()))
+                            .collect(Collectors.joining("\n"));
+
+                    ((TextView) cv.findViewById(R.id.tv_day_content)).setText(str);
+                }
+
+                cal.add(Calendar.DAY_OF_YEAR, 1);
+            }
+        }
+
+        private List<Course> filterCourseByDay(List<Course> pCourses, long midnightTS){
+            return pCourses.stream().filter(course ->
+                    course.getDate() >= midnightTS && course.getDate() < (midnightTS + ONE_DAY))
+                    .collect(Collectors.toList());
         }
     }
 }
